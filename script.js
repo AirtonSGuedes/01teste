1567e246dd6e44c353137057550689f0481ea869
AirtonSGuedes <113541135+AirtonSGuedes@users.noreply.github.com>
2023-05-07 22:04:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function menuOnClick() {
     document.querySelector('.nav-list').classList.toggle('active');
     document.getElementById('menu-bar').classList.toggle('change');
-    
-  }
+    document.getElementsByClassName("nav-list")[1].classList.toggle("active");
+}
+
   
